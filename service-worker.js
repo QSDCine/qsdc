fd92457715b4b59d1380461c55ba1e5966e276b4
QSDCine <quesabesdecine@gmail.com>
2025-04-15 01:11:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -42,22 +42,14 @@ self.addEventListener('activate', (e) => {
 // Fetch a servidor
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // Si hay en la caché (qsdcine o qsdcine-core-v1), úsalo
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      // Si no está cacheado, intenta desde red
-      return fetch(event.request).catch(() => {
-        // Si no hay red, y es un audio, muestra un error 
-        if (event.request.destination === "audio") {
-          return new Response("", { status: 404, statusText: "Audio no disponible offline" });
-        }
-        return new Response("Offline", {
-          status: 503,
-          statusText: "Offline"
-        });
+    caches.match(event.request, { ignoreSearch: true }).then((response) => {
+      if (response) return response;
+      return fetch(event.request);
+    }).catch((error) => {
+      console.error("[SW] Fetch failed:", error);
+      return new Response("Archivo no disponible offline", {
+        status: 503,
+        statusText: "Offline y sin caché",
       });
     })
   );
